5447ea22dd7b13fd0a993bc5cf436566bd54f739
EL OUAZINE Youssef Ext OBS/CSO <RKFF4155@orange.com>
2020-12-04 01:27:42 +0100

    Edit autoloader file

--- Autoloader.php
+++ Autoloader.php
@@ -5,7 +5,7 @@ class Autoloader
     {
         spl_autoload_register(function ($class) {
             
-            $file = "C:/wamp64/www/jeu carte/jeu-de-carte-php/".$class.'.php';
+            $file = "../".$class.'.php';
             if (file_exists($file)) {
                 require_once($file);
                 return true;
